265d953884a634e28637ad02c322ad65029f36bd
Chamath Wickramashinghe <wickrachamath@gmail.com>
2025-05-17 16:27:46 +0530

    Metro Config

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -3,9 +3,9 @@ import { Stack } from 'expo-router';
 export default function AuthLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="splash" options={{ headerShown: false }} />
-      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
-      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
-    </Stack>
+      <Stack.Screen name="splash" options={{ headerShown: false, title: 'SplashScreen' }} />
+      <Stack.Screen name="sign-in" options={{ headerShown: false, title: 'SignInScreen' }} />
+      <Stack.Screen name="sign-up" options={{ headerShown: false, title: 'SignUpScreen' }} />
+    </Stack> 
   );
 }
